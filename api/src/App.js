e690f8759099cbd82b098f7fa75f3c0515faf855
vinothsha <kvinothsha@gmail.com>
2022-03-16 14:00:35 +0530

    initial

---
+++ api/src/App.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+
+function GetAxios() {
+    const[posts,setPost] = useState([])
+    useEffect(()=>{
+         axios.get(`http://localhost:8080/getstudents`)
+
+        .then(res=>{
+            setPost(JSON.stringify(res.data))
+            // var con=res.data
+            // setPost(con)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
+  return(<div>                            
+      <h1>{posts}</h1>
+  </div>)
+}
+
+export default GetAxios;
